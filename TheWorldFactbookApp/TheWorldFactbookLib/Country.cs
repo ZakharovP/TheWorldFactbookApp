b25b9dfa79bd5e1c47a2a555d7c65e5166ff1cab
ZakharovP <45815754+ZakharovP@users.noreply.github.com>
2019-04-05 19:09:12 +0300

    add country class

---
+++ TheWorldFactbookApp/TheWorldFactbookLib/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorldFactbookLib
+{
+    public class Country
+    {
+        public string Name;
+        public string Capital;
+        public Geography Geography;
+        public Population Population;
+        public Economy Economy;
+    }
+    public class Geography
+    {
+        public long TotalArea;  
+    }
+    public class Population
+    {
+        public long Amount;
+    }
+    public class Economy
+    {
+        public long GDPtotal;
+        public long GDPppp;
+    }
+}
+
